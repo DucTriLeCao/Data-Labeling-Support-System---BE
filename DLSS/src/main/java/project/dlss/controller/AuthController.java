8a6ea6d9066aeb30e129cc0d15b4969e5cb5b75e
Akira174 <tuanpg2016@gmail.com>
2026-03-07 12:56:35 +0700

    Authen updated

--- DLSS/src/main/java/project/dlss/controller/AuthController.java
+++ DLSS/src/main/java/project/dlss/controller/AuthController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import project.dlss.dto.LoginRequestDTO;
 import project.dlss.dto.LoginResponseDTO;
+import project.dlss.dto.RegisterRequestDTO;
 import project.dlss.service.AuthService;
 
 @RestController
@@ -13,11 +14,19 @@ public class AuthController {
 
     private final AuthService authService;
 
+    // LOGIN
     @PostMapping("/login")
     public LoginResponseDTO login(@RequestBody LoginRequestDTO request) {
         return authService.login(request);
     }
 
+    // REGISTER
+    @PostMapping("/register")
+    public String register(@RequestBody RegisterRequestDTO request) {
+        return authService.register(request);
+    }
+
+    // LOGOUT
     @PostMapping("/logout")
     public String logout() {
         return authService.logout();
